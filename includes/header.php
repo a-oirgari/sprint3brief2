07fa20fecf16ab30453a68821025f906c2361ec2
a-oirgari <abdrahmanghali1998@gmail.com>
2025-12-17 12:19:57 +0100

    the cdn for font awsome was added

--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <title>Unity Care Clinic - Admin</title>
 
+    
     <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body class="bg-gray-100">
+
+
+
 
 <div class="container mx-auto">
